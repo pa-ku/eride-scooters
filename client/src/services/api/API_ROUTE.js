e99f3c761dca7c729541e1c506a25c5e947e1bb2
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-03-12 20:18:23 -0300

    updated api route

--- client/src/services/api/API_ROUTE.js
+++ client/src/services/api/API_ROUTE.js
@@ -1,2 +1,4 @@
 export const API_ROUTE =
-  process.env.NEXT_PUBLIC_API_ROUTE || 'http://localhost:3000/api'
+  process.env.VITE_API_ROUTE === 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://eride-api.vercel.app/api'
