42ce6f5d210fe8050e833e41039b729103fd0864
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-03-12 17:05:17 -0300

    modified vercel.json

--- client/src/services/api/API_ROUTE.js
+++ client/src/services/api/API_ROUTE.js
@@ -1,3 +1,1 @@
-export const API_ROUTE = import.meta.env.VITE_DEVELOPMENT
-  ? 'http://localhost:3000/api'
-  : 'https://eride-api.vercel.app/api'
+export const API_ROUTE = import.meta.env.VITE_API_ROUTE
