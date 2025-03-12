541ca33aead4aed8e20844c1275e8fe8f50aed5b
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-03-12 20:30:15 -0300

    updated env mode

--- client/src/services/api/API_ROUTE.js
+++ client/src/services/api/API_ROUTE.js
@@ -1,4 +1,4 @@
 export const API_ROUTE =
-import.meta.env.VITE_API_ENV === 'development'
+  import.meta.env.MODE === 'development'
     ? 'http://localhost:3000/api'
     : 'https://eride-api.vercel.app/api'
